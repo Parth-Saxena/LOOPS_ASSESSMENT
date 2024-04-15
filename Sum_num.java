80a5b9ca0b957708e9c2707d2aff8f85e199c0d1
Parth Saxena <parthsaxena2003.ps@gmail.com>
2024-04-15 23:21:06 +0530

    first commit

---
+++ Sum_num.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Sum_num
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a positive number: ");
+        int num =scan.nextInt();
+        if(num<=0)
+        {
+            System.out.println("Please,enter a positive number: ");
+            num = scan.nextInt();
+        }
+        else
+        {
+            int sum=0;
+            for(int i=0;i<=num;i++)
+            {
+                sum=sum+i;
+            }
+            System.out.println("The sum of " + num + "positive numbers is: " + sum );
+        }
+        scan.close();
+        
+    }
+}
